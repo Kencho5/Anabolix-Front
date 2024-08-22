8fdf7a88c18efb41e3043f6bf018dbfe80b3c905
Kencho5 <giokenchadze@gmail.com>
2024-08-22 13:09:15 +0400

    Removed: auth guard on home

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,19 +1,12 @@
 import { Router, Route } from "preact-router";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import AuthGuard from "../auth/AuthGuard";
+// import AuthGuard from "../auth/AuthGuard";
 
 const AppRoutes = () => {
   return (
     <Router>
-      <Route
-        path="/"
-        component={() => (
-          <AuthGuard>
-            <Home />
-          </AuthGuard>
-        )}
-      />
+      <Route path="/" component={Home} />
       <Route path="/login" component={Login} />
     </Router>
   );
