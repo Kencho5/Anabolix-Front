1ea85c89cfdcda507ef6876f78883e0c4208845b
Kencho5 <giokenchadze@gmail.com>
2024-08-22 13:05:28 +0400

    Added: guard on home for test

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,12 +1,19 @@
 import { Router, Route } from "preact-router";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-// import AuthGuard from "../auth/AuthGuard";
+import AuthGuard from "../auth/AuthGuard";
 
 const AppRoutes = () => {
   return (
     <Router>
-      <Route path="/" component={Home} />
+      <Route
+        path="/"
+        component={() => (
+          <AuthGuard>
+            <Home />
+          </AuthGuard>
+        )}
+      />
       <Route path="/login" component={Login} />
     </Router>
   );
