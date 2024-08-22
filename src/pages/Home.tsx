d61163c99777642e386f731fd48b2b3720a4c4c9
Kencho5 <giokenchadze@gmail.com>
2024-08-22 12:46:01 +0400

    Test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
-import Posts from "../components/Posts";
-import { useAuth } from "../auth/AuthContext";
+// import Posts from "../components/Posts";
+// import { useAuth } from "../auth/AuthContext";
+//
+// const Home = () => {
+//   const { loggedIn } = useAuth();
+//
+//   return (
+//     <div className="mx-auto mt-6 flex max-w-3xl flex-col items-center gap-2 rounded-xl bg-stone-200 p-4">
+//       <h3 className="mb-2 text-2xl font-bold text-stone-900">Home</h3>
+//       <img
+//         src="/avatar.jpg"
+//         width={128}
+//         height={128}
+//         className="h-[128px] w-[128px] rounded-xl"
+//         alt=""
+//       />
+//       {loggedIn ? (
+//         <p className="mb-4 text-lg text-green-600">Authenticated</p>
+//       ) : (
+//         <p className="mb-4 text-lg text-red-600">Unauthorized</p>
+//       )}
+//       <Posts />
+//     </div>
+//   );
+// };
+//
+// export default Home;
 
 const Home = () => {
-  const { loggedIn } = useAuth();
-
   return (
     <div className="mx-auto mt-6 flex max-w-3xl flex-col items-center gap-2 rounded-xl bg-stone-200 p-4">
       <h3 className="mb-2 text-2xl font-bold text-stone-900">Home</h3>
@@ -14,12 +37,6 @@ const Home = () => {
         className="h-[128px] w-[128px] rounded-xl"
         alt=""
       />
-      {loggedIn ? (
-        <p className="mb-4 text-lg text-green-600">Authenticated</p>
-      ) : (
-        <p className="mb-4 text-lg text-red-600">Unauthorized</p>
-      )}
-      <Posts />
     </div>
   );
 };
