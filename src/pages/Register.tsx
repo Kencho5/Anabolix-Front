089782358344e702eb0b1d97c13f1cc0c9b81dc4
Kencho5 <giokenchadze@gmail.com>
2024-09-11 14:46:59 +0400

    Added: not found page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,50 +1,40 @@
 import { useReducer } from "react";
 import { initialState, reducer } from "../utils/authReducer";
-import { useAuth } from "../auth/AuthContext";
 import { Link } from "react-router-dom";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const Login = () => {
+const Register = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { login } = useAuth();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     dispatch({ type: "SET_LOADING", payload: true });
 
-    try {
-      const response = await fetch(`${API_URL}/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: state.username,
-          password: state.password,
-        }),
-      });
+    const response = await fetch(`${API_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: state.username,
+        password: state.password,
+      }),
+    });
 
-      if (response.ok) {
-        const { token } = await response.json();
-        await login(token);
-
-        dispatch({ type: "SET_STATUS", payload: "success" });
-      } else {
-        dispatch({ type: "SET_STATUS", payload: "error" });
-      }
-    } catch (error) {
+    if (response.ok) {
+      dispatch({ type: "SET_STATUS", payload: "success" });
+    } else {
       dispatch({ type: "SET_STATUS", payload: "error" });
-    } finally {
-      dispatch({ type: "SET_LOADING", payload: false });
     }
+    dispatch({ type: "SET_LOADING", payload: false });
   };
 
   return (
     <div className="flex min-h-screen flex-col items-center bg-stone-100">
       <div className="mt-10 w-full max-w-sm transform rounded-lg bg-white p-8 shadow-lg">
         <h3 className="mb-6 text-center text-3xl font-bold text-gray-800">
-          Login
+          Register
         </h3>
         <form onSubmit={handleSubmit} className="space-y-6">
           <div>
@@ -92,13 +82,13 @@ const Login = () => {
 
           {state.status === "error" && (
             <div className="mb-4 rounded-md bg-red-100 p-4 text-red-600">
-              <p>Incorrect login information.</p>
+              <p>Username taken.</p>
             </div>
           )}
 
           {state.status === "success" && (
             <div className="mb-4 rounded-md bg-green-100 p-4 text-green-600">
-              <p>Login successful!</p>
+              <p>Registration successful!</p>
             </div>
           )}
 
@@ -110,16 +100,16 @@ const Login = () => {
                 : "hover:scale-105 hover:bg-teal-700"
             }`}
           >
-            {state.isLoading ? "Submitting..." : "Login"}
+            {state.isLoading ? "Submitting..." : "Register"}
           </button>
 
           <div className="mt-4 text-center text-sm text-gray-600">
-            Don't have an account?{" "}
+            Have an account?{" "}
             <Link
-              to="/register"
+              to="/login"
               className="font-semibold text-teal-600 transition duration-200 hover:text-teal-700"
             >
-              Register
+              Log in
             </Link>
           </div>
         </form>
@@ -128,4 +118,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
 
 // import ProtectedRoute from "../auth/AuthGuard";
 
@@ -10,6 +12,9 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
