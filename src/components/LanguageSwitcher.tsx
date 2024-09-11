4762faec2984fc836f020b7b36c959226197954a
Kencho5 <giokenchadze@gmail.com>
2024-09-11 22:29:53 +0400

    Added: outside click handlert

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { useState } from "react";
+import OutsideClickHandler from "../utils/OutsideClick";
 
 function LanguageSwitcher() {
   const { i18n } = useTranslation();
@@ -17,33 +18,35 @@ function LanguageSwitcher() {
   };
 
   return (
-    <div className="relative inline-block text-left">
-      <button
-        type="button"
-        onClick={() => setIsOpen(!isOpen)}
-        className="rounded-lg bg-gray-700 px-4 py-2 text-white focus:outline-none"
-      >
-        {language === "en" ? "English" : "Georgian"}
-      </button>
-      {isOpen && (
-        <div className="absolute right-0 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-          <div className="py-1">
-            <button
-              onClick={() => changeLanguage("en")}
-              className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
-            >
-              English
-            </button>
-            <button
-              onClick={() => changeLanguage("ge")}
-              className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
-            >
-              Georgian
-            </button>
+    <OutsideClickHandler onOutsideClick={() => setIsOpen(false)}>
+      <div className="relative inline-block text-left">
+        <button
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+          className="rounded-lg bg-gray-700 px-4 py-2 text-white focus:outline-none"
+        >
+          {language === "en" ? "English" : "Georgian"}
+        </button>
+        {isOpen && (
+          <div className="absolute right-0 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="py-1">
+              <button
+                onClick={() => changeLanguage("en")}
+                className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+              >
+                English
+              </button>
+              <button
+                onClick={() => changeLanguage("ge")}
+                className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100"
+              >
+                Georgian
+              </button>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </OutsideClickHandler>
   );
 }
 
