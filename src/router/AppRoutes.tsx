7378fd9044ebac449f8336a084ff3b7de3534fbc
Kencho5 <giokenchadze@gmail.com>
2024-09-15 14:43:12 +0400

    Added: prefetching pages

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,15 +1,22 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import TopBarProgress from "react-topbar-progress-indicator";
 
 import Home from "../pages/Home";
-import Login from "../pages/Login";
+
+// Lazy load components
+const Login = lazy(() => import("../pages/Login"));
 const Register = lazy(() => import("../pages/Register"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 
-// import ProtectedRoute from "../auth/AuthGuard";
-
 const AppRoutes = () => {
+  useEffect(() => {
+    // Preload components
+    import("../pages/Login");
+    import("../pages/Register");
+    import("../pages/NotFound");
+  }, []);
+
   return (
     <Suspense fallback={<TopBarProgress />}>
       <Routes>
