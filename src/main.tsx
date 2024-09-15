12039a440d38bde6ceffd8b9541890eff19f1535
Kencho5 <giokenchadze@gmail.com>
2024-09-15 12:55:06 +0400

    Added: lazy loading and top loading bar

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./app";
 import "./index.css";
 import "./i18n";
+import TopBarProgress from "react-topbar-progress-indicator";
+
+TopBarProgress.config({
+  barColors: {
+    "0": "#0d9488",
+    "1.0": "#0d9488",
+  },
+  shadowBlur: 2,
+  barThickness: 3,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,21 +1,24 @@
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
+import TopBarProgress from "react-topbar-progress-indicator";
 
 import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import NotFound from "../pages/NotFound";
+const Login = lazy(() => import("../pages/Login"));
+const Register = lazy(() => import("../pages/Register"));
+const NotFound = lazy(() => import("../pages/NotFound"));
 
 // import ProtectedRoute from "../auth/AuthGuard";
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={<TopBarProgress />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 };
 
